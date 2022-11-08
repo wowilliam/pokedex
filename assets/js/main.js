0bc0142be87e6eca019e852a45bf476ff86f3137
wowilliam <wo.william@hotmail.com>
2022-11-07 22:46:13 -0300

    projeto Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
 
- const offset = 0
- const limit = 10
+const offset = 0
+const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
- fetch(url).then(function (response) {
-    console.log(response)
- })
+function convertPokemonToHtml(pokemon) {
+   return `
+   <li class="pokemon">
+   <span class="number">001</span>
+   <span class="nome">${pokemon.name}</span>
 
- const x = 10 + 10
- console.log(x)
+   <div class="detail">
+       <ol class="types">
+           <li class="type">grass</li>
+           <li class="type">poison</li>
+       </ol>
+       <img src="https://static.wikia.nocookie.net/pokedex/images/2/24/Bubassaur.png/revision/latest?cb=20141012190624&path-prefix=pt-br"
+           alt="${pokemon.name}">
+   </div>
+</li>
+   
+   `
+
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+   .then((response) => response.json())
+   .then((jsonBody) => jsonBody.results)
+   .then((pokemons) => {
+     
+     for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+      
+     }     
+      
+   })
+   .catch((error) => console.error(error))
+
+
+  
