bb2f1a420559eb078b566f66afc7e0ce85d17550
wowilliam <wo.william@hotmail.com>
2022-11-07 23:13:21 -0300

    projeto Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
    return `
    <li class="pokemon">
    <span class="number">001</span>
@@ -25,19 +25,16 @@ function convertPokemonToHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-   .then((response) => response.json())
-   .then((jsonBody) => jsonBody.results)
-   .then((pokemons) => {
-     
-     for (let i = 0; i < pokemons.length; i++) {
+pokeApi.getPokemons().then((pokemons) => {
+   const listItems = []
+
+   for (let i = 0; i < pokemons.length; i++) {
       const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-      
-     }     
-      
-   })
-   .catch((error) => console.error(error))
+      listItems.push(convertPokemonToLi(pokemon))
+
+   }
+   console.log(listItems)
+})
 
 
-  
+
