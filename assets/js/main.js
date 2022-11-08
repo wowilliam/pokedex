453d25eb6c79d363aeda224a843588391681852d
wowilliam <wo.william@hotmail.com>
2022-11-08 14:00:06 -0300

    projeto Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypeToLi(pokemonTypes) {
+   return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
    return `
@@ -11,10 +11,9 @@ function convertPokemonToLi(pokemon) {
 
    <div class="detail">
        <ol class="types">
-           <li class="type">grass</li>
-           <li class="type">poison</li>
+          ${convertPokemonTypeToLi(pokemon.types).join('')}
        </ol>
-       <img src="https://static.wikia.nocookie.net/pokedex/images/2/24/Bubassaur.png/revision/latest?cb=20141012190624&path-prefix=pt-br"
+       <img src="${pokemon.sprites.other.dream_world.front_default}"
            alt="${pokemon.name}">
    </div>
 </li>
@@ -25,16 +24,10 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-   const listItems = []
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 
-   for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      listItems.push(convertPokemonToLi(pokemon))
-
-   }
-   console.log(listItems)
-})
+ })
 
 
 
